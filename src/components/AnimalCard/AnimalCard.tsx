d4559d84375a81121e0f56ea0a09267609893cdf
Pavel Movchan <tz_95@mail.ru>
2022-06-03 15:59:54 +0900

    formated with prettier

--- src/components/AnimalCard/AnimalCard.tsx
+++ src/components/AnimalCard/AnimalCard.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import {AnimalCardProps} from '../types/types'
+import React from 'react';
+import { AnimalCardProps } from '../types/types';
 
-import './AnimalCard.css'
+import './AnimalCard.css';
 
-const AnimalCard: React.FC<AnimalCardProps> = ({img, name, feature, removeAnimal, feedAnimal}) => {
+const AnimalCard: React.FC<AnimalCardProps> = ({
+  img,
+  name,
+  feature,
+  removeAnimal,
+  feedAnimal,
+}) => {
   return (
-    <div className='animal-card-container'>
-      <div className='animal-card-container__info'>
+    <div className="animal-card-container">
+      <div className="animal-card-container__info">
         <img src={img} alt={name}></img>
-        <div className='animal-card-container__title'>
+        <div className="animal-card-container__title">
           <span>{name}</span>
-          <span>{feature.name}:{feature.count}</span>
+          <span>
+            {feature.name}:{feature.count}
+          </span>
         </div>
       </div>
-          <div className='animal-card-container__buttons'>
-          <button onClick={feedAnimal}>Feed</button>
-          <button onClick={removeAnimal}>Free</button>
-          </div>
-        </div>
-  )
-}
+      <div className="animal-card-container__buttons">
+        <button onClick={feedAnimal}>Feed</button>
+        <button onClick={removeAnimal}>Free</button>
+      </div>
+    </div>
+  );
+};
 
-export default AnimalCard
+export default AnimalCard;
--- src/components/types/types.tsx
+++ src/components/types/types.tsx
@@ -1,7 +1,7 @@
 export interface AnimalCardProps {
   img: string;
   name: string;
-  feature: {name: string; count: number;};
+  feature: { name: string; count: number };
   removeAnimal?: () => void;
   feedAnimal?: () => void;
-};
+}
